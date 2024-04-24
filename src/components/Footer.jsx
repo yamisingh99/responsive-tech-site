5964c61c443da166f478724b7fc5e415ad3fc8f7
YAMINI SINGH <122320151+yamisingh99@users.noreply.github.com>
2024-04-24 17:22:23 +0530

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Yashichauhan99</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCAOj3RVHVh-wMS6drc-Ynzg" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/yashichauhan99/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/yamisingh99" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Web Dev</h1>
+          <p>Solutions for all of your problems in tech web-dev</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+            officiis hic illo quos sit commodi, aut magnam odio eligendi? Totam
+            quis officiis fugit sunt dolores nostrum tenetur iusto est odio
+            mollitia. Dolor placeat repellendus officia aspernatur dolorum
+            harum. Dolorem cumque quaerat eius qui deserunt ullam laudantium
+            quia tempore. Dignissimos esse sequi numquam est ipsam nemo,
+            voluptas vel recusandae iste aspernatur qui quasi, commodi, atque
+            ipsa dolores tempore asperiores? Dolores soluta placeat molestiae
+            aperiam, ullam officia fugiat corrupti odio, suscipit ipsum cum vel
+         
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
